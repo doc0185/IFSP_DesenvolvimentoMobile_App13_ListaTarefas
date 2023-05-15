3e3338ce9be87a1a284df6daa3aa286cd264d534
usuario-pc <stefanofog@gmail.com>
2023-05-15 10:54:32 -0300

    Versão Inicial Visual, apenas layouts

---
+++ app/src/main/java/br/edu/ifsp/dmo/app13_lista_de_tarefas/view/TasksDetailsActivity.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp.dmo.app13_lista_de_tarefas.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.ifsp.dmo.app13_lista_de_tarefas.R;
+
+public class TasksDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newtask);
+    }
+}
