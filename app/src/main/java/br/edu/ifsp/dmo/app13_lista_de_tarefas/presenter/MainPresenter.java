7921e6e6b605ed87c80a3be44840994a1c8590b7
stefa <stefanofog@gmail.com>
2023-05-16 18:20:20 -0300

    Início Presenter

---
+++ app/src/main/java/br/edu/ifsp/dmo/app13_lista_de_tarefas/presenter/MainPresenter.java
@@ -0,0 +1,66 @@
+package br.edu.ifsp.dmo.app13_lista_de_tarefas.presenter;
+
+import android.content.Intent;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import br.edu.ifsp.dmo.app13_lista_de_tarefas.model.DAO.ITaskDAO;
+import br.edu.ifsp.dmo.app13_lista_de_tarefas.model.DAO.TaskDAOSingleton;
+import br.edu.ifsp.dmo.app13_lista_de_tarefas.model.Entities.Task;
+import br.edu.ifsp.dmo.app13_lista_de_tarefas.mvp.MainMVP;
+import br.edu.ifsp.dmo.app13_lista_de_tarefas.utils.Constant;
+import br.edu.ifsp.dmo.app13_lista_de_tarefas.view.TasksDetailsActivity;
+
+public class MainPresenter implements MainMVP.Presenter{
+    private MainMVP.View view;
+    private ITaskDAO dao;
+    Task task;
+
+    public MainPresenter(MainMVP.View view) {
+        this.view = view;
+        dao = TaskDAOSingleton.getInstance();
+    }
+
+
+    @Override
+    public void deatach() {
+        view = null;
+    }
+
+    @Override
+    public void openDetails() {
+        Intent intent = new Intent(view.getContext(), TasksDetailsActivity.class);
+        view.getContext().startActivity(intent);
+    }
+
+    @Override
+    public void openDetails(Task task) {
+        Intent intent = new Intent(view.getContext(), TasksDetailsActivity.class);
+        intent.putExtra(Constant.ATTR_TITLE, task.getTitle());
+        view.getContext().startActivity(intent);
+    }
+
+    @Override
+    public void populateList(RecyclerView recyclerView) {
+        /*
+        ItemPocketRecyclerAdapter adapter = new
+                ItemPocketRecyclerAdapter(view.getContext(), dao.findAll(), this);
+        adapter.setClickListener(new RecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                task = dao.findAll().get(position);
+                openDetails(article);
+            }
+        });
+        RecyclerView.LayoutManager layoutManager = new
+                LinearLayoutManager(view.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);*/
+    }
+
+    @Override
+    public void favoriteTask(Task task) {
+        task.setImportant(!task.isImportant());
+        dao.update(task.getTitle(), task);
+    }
+}
---
+++ app/src/main/java/br/edu/ifsp/dmo/app13_lista_de_tarefas/utils/Constant.java
@@ -0,0 +1,7 @@
+package br.edu.ifsp.dmo.app13_lista_de_tarefas.utils;
+
+public class Constant {
+    public static final String ENTITY_TASK = "task";
+    public static final String ATTR_TITLE = "title";
+    public static final String ATTR_DESC = "description";
+}
